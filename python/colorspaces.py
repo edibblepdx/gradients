820c9bc708572d92bc51c81e87d17bb51311baf0
ethan dibble <edibble@pdx.edu>
2025-02-24 22:26:53 -0800

    format

--- python/colorspaces.py
+++ python/colorspaces.py
@@ -15,6 +15,7 @@ import numpy as np
 
 type Vector = list[float]
 
+
 def rgb_to_gray(rgb: Vector) -> Vector:
     """
     Input: [0,1]
@@ -24,26 +25,31 @@ def rgb_to_gray(rgb: Vector) -> Vector:
 
     return [intensity] * 3
 
+
 def rgb_to_xyz(rgb: Vector) -> Vector:
     """
     Input: [0,1]
     Output: [0,1]
     """
-    # RGB to XYZ conversion matrix D65 white point 
-    M = np.array([[0.412453, 0.357580, 0.180423],
-                  [0.212671, 0.715160, 0.072169],
-                  [0.019334, 0.119193, 0.950227]])
+    # RGB to XYZ conversion matrix D65 white point
+    M = np.array(
+        [
+            [0.412453, 0.357580, 0.180423],
+            [0.212671, 0.715160, 0.072169],
+            [0.019334, 0.119193, 0.950227],
+        ]
+    )
 
     return np.dot(M, rgb).tolist()
+
 
 def rgb_to_hsv(rgb: Vector) -> Vector:
     """
     Input: [0,1]
     Output: 0≤V≤1, 0≤S≤1, 0≤H≤360
     """
-    min_val = min(rgb)
-
-    v = max(rgb)
+    v_max = max(rgb)
+    v_min = min(rgb)
 
     s = (0.0) if (v == 0) else ((v - min_val) / v)
 
@@ -58,7 +64,8 @@ def rgb_to_hsv(rgb: Vector) -> Vector:
     if h < 0:
         h = h + 360.0
 
-    return [h, s, v]
+    return [h, s, v_max]
+
 
 def rgb_to_hls(rgb: Vector) -> Vector:
     """
@@ -87,26 +94,33 @@ def rgb_to_hls(rgb: Vector) -> Vector:
 
     return [h, l, s]
 
+
 def xyz_to_rgb(xyz: Vector) -> Vector:
-    # inverse RGB to XYZ conversion matrix D65 white point 
-    M_inv = np.array([[ 3.240479, -1.53715,  -0.498535],
-                      [-0.969256,  1.875991,  0.041556],
-                      [ 0.055648, -0.204043,  1.057311]])
+    # inverse RGB to XYZ conversion matrix D65 white point
+    M_inv = np.array(
+        [
+            [3.240479, -1.53715, -0.498535],
+            [-0.969256, 1.875991, 0.041556],
+            [0.055648, -0.204043, 1.057311],
+        ]
+    )
 
     return np.clip(np.dot(M_inv, xyz), 0, 1).tolist()
+
 
 def xyz_to_lab(xyz: Vector) -> Vector:
     lab = np.zeros(3)
     X = xyz[0] / 0.950455
     Y = xyz[1] / 1.0
     Z = xyz[2] / 1.088753
-    f = lambda t : t**(1 / 3) if t > 0.008856 else 7.787 * t + (16 / 116)
+    f = lambda t: t ** (1 / 3) if t > 0.008856 else 7.787 * t + (16 / 116)
 
     lab[0] = 116 * f(Y) - 16 if Y > 0.008856 else 903.3 * Y
     lab[1] = 500 * (f(X) - f(Y))
     lab[2] = 200 * (f(Y) - f(Z))
 
     return lab.tolist()
+
 
 def xyz_to_luv(xyz: Vector) -> Vector:
     """
@@ -115,19 +129,21 @@ def xyz_to_luv(xyz: Vector) -> Vector:
     """
     pass
 
+
 # this is weird weird
 def lerp(colorStart: Vector, colorEnd: Vector, u: int) -> Vector:
     return (1 - u) * colorStart + u * colorEnd
+
 
 if __name__ == "__main__":
     colorStart = np.array([0.5, 0, 0])
     colorEnd = np.array([1, 1, 1])
 
-    print(f"{'sRGB':24}", 'L*a*b*')
-    print('-' * 70)
+    print(f"{'sRGB':24}", "L*a*b*")
+    print("-" * 70)
     for u in [0, 0.25, 0.5, 0.75, 1]:
         color = lerp(colorStart, colorEnd, u)
-        print(f"{np.array_str(color):20}", '\t', xyz_to_lab(rgb_to_xyz(color)))
+        print(f"{np.array_str(color):20}", "\t", xyz_to_lab(rgb_to_xyz(color)))
 
     for i in range(1):
         r, g, b = i, 255 - i, 128  # Example gradient logic
